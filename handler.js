57048ae176d3c451519b74c750c64fe3e1b9e3ea
david cash <david.w.cash@gmail.com>
2021-01-25 21:51:21 -0600

    Setup Dynamo DB and default Region to us-east-1

--- handler.js
+++ handler.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+AWS.config.update({region: "us-east-1"});
+
 const sqs = new AWS.SQS({apiVersion: "2012-11-05"});
+const dynamodb = new AWS.DynamoDB({apiVersion: "2012-08-10"});
 
 module.exports.helloWorld = (event, context, callback) => {
 
