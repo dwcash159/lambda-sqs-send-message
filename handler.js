24cef2686a929ed8f9d34b9589cdbb78ab77b040
david cash <david.w.cash@gmail.com>
2021-01-26 06:35:13 -0600

    WIP - Dynamo DB insert

--- handler.js
+++ handler.js
@@ -4,7 +4,9 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: "us-east-1"});
 
 const sqs = new AWS.SQS({apiVersion: "2012-11-05"});
-const dynamodb = new AWS.DynamoDB({apiVersion: "2012-08-10"});
+
+// Create the document client interface for DynamoDB
+var documentClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.helloWorld = (event, context, callback) => {
 
@@ -15,8 +17,7 @@ module.exports.helloWorld = (event, context, callback) => {
     QueueUrl: 'https://sqs.us-east-1.amazonaws.com/615027074482/aws-communications-queue'
   };
 
-
-  const response = {
+  var response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
@@ -27,13 +28,37 @@ module.exports.helloWorld = (event, context, callback) => {
     }),
   };
 
+  var dynamoDbPayload = {
+    TableName: "communications-sqs-db",
+    Item: json
+  };
+
+
+
   const sqsdata = sqs.sendMessage(sqsParams, function(err, data) {
+    var body = JSON.parse(response.body);
     if (err) {
       console.log('ERR', err);
-      response.statusCode = 502;
+
+      response.statusCode = 500;
+      response.body = JSON.stringify(body);
       callback(null, response);
     }
     console.log(data);
+    body.messageId = data.MessageId;
+    response.body = JSON.stringify(body);
+    dynamoDbPayload.Item.messageId = data.MessageId;
+
+    documentClient.put(params, function(err, data) {
+      if (err) {
+        response.statusCode = 500;
+        response.body = JSON.stringify(body);
+        callback(null, response);
+      } else {
+        console.log("Succeeded adding communicatiion");
+      }
+    });
+
     callback(null, response);
   });
 };
